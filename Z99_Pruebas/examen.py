f05cb1a5f02b6b0ec0986c5b1caebdbbe4bcc35e
Javier Sanchez Padilla <javier.sp@gmail.com>
2026-01-28 20:28:21 -0600

    Eliminaos en venv y estamos por subir a github

---
+++ Z99_Pruebas/examen.py
@@ -0,0 +1,9 @@
+def area_cuadrado(lado):					
+    for base in lado:	
+        print(base)		
+
+lados_figura = ['Juan', 'Pedro', 'karla', 'Ana', 'Luis']
+area_cuadrado(lados_figura)
+area_cuadrado((15, 21, 0))    
+area_cuadrado([9, 13, 1, -2])     
+area_cuadrado({29, 213, 21, 22})     
